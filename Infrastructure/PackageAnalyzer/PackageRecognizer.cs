74baa193a3d7bd989e625b7d92e43d241d62873f
NeutraleNull <oliverkrue@googlemail.com>
2024-04-03 22:54:16 +0200

    reworked

--- Infrastructure/PackageAnalyzer/PackageRecognizer.cs
+++ Infrastructure/PackageAnalyzer/PackageRecognizer.cs
@@ -24,7 +24,8 @@ public class PackageRecognizer(IServiceProvider serviceProvider)
 
         var featureExtractor = new JavascriptFeatureExtractor();
 
-        var functionSignatures = new List<FunctionSignature>();
+        var allFoundMinHashes = new Dictionary<string, List<(string? Namespace, string LibName, string Version, double Similarity)>>();
+        var allFoundSimHashes = new Dictionary<string, List<(string? Namespace, string LibName, string Version, double Similarity)>>();
         
         foreach (var file in files)
         {
@@ -32,102 +33,104 @@ public class PackageRecognizer(IServiceProvider serviceProvider)
             var extractFeatures = featureExtractor.ExtractFeatures(code, file, HelperFunctions.IsModule(file, code))
                 .Where(x => x.ExtractedFeatures.Count > extractionThreshold)
                 .ToList();
-
-            functionSignatures.AddRange(extractFeatures.Select(x => new FunctionSignature
+            
+            foreach (var feature in extractFeatures)
             {
-                FunctionName = x.FunctionName,
-                SignatureMinhash = MinHash.ComputeMinHash(x.ExtractedFeatures.Select(y => y.data).ToList()),
-                SignatureSimhash = SimHash.ComputeSimHash(x.ExtractedFeatures, Weights.DefaultWeights)
-            }));
+                var signatureMinHash = MinHash.ComputeMinHash(feature.ExtractedFeatures.Select(x => x.data).ToList());
+                var signatureSimHash = SimHash.ComputeSimHash(feature.ExtractedFeatures, Weights.DefaultWeights);
+
+                var similarSimHashes = DataSet
+                    .AsParallel()
+                    .Where(x => SimHash.SimilarityPercentage(x.SignatureSimhash, signatureSimHash) > minSimilarity)
+                    .ToList();
+                var similarMinHashes = DataSet
+                    .AsParallel()
+                    .Where(x => MinHash.GetSimilarity(x.SignatureMinhash, signatureMinHash) > minSimilarity)
+                    .ToList();
+
+                foreach (var functionSignature in similarSimHashes)
+                {
+                    var similarity = SimHash.SimilarityPercentage(functionSignature.SignatureSimhash, signatureSimHash);
+                    var libName = functionSignature.LibName;
+                    var namespaceName = functionSignature.Namespace;
+                    var version = functionSignature.Version;
+                    
+                    if (!allFoundSimHashes.ContainsKey(libName))
+                    {
+                        allFoundSimHashes[libName] = [];
+                    }
+
+                    allFoundSimHashes[libName].Add((namespaceName, libName, version, similarity));
+                }
+                
+                foreach (var functionSignature in similarMinHashes)
+                {
+                    var similarity = MinHash.GetSimilarity(functionSignature.SignatureMinhash, signatureMinHash);
+                    var libName = functionSignature.LibName;
+                    var namespaceName = functionSignature.Namespace;
+                    var version = functionSignature.Version;
+                    
+                    if (!allFoundMinHashes.ContainsKey(libName))
+                    {
+                        allFoundMinHashes[libName] = [];
+                    }
+                    allFoundMinHashes[libName].Add((namespaceName, libName, version, similarity));
+                }
+            }
         }
+
+        var mostLikelyVersionsMinHashes = GetMostLikelyVersions(allFoundMinHashes, 5);
+        var mostLikelyVersionsSimHashes = GetMostLikelyVersions(allFoundSimHashes, 5);
         
-        var similarByMinHash = await FindSimilarMinHash(functionSignatures, minSimilarity, cancellationToken);
-        var similarBySimHash = await FindSimilarSimHash(functionSignatures, minSimilarity, cancellationToken);
-
-        var minHashSolution = FilterMatches(similarByMinHash, 5);
-        var simHashSolution = FilterMatches(similarBySimHash, 5);
-
         Console.WriteLine("===== Folder: {0} =======", folderDirectory);
         Console.WriteLine("~~~~~ MINHASH ~~~~~~~");
-        foreach (var element in minHashSolution)
+        foreach (var element in mostLikelyVersionsMinHashes)
         {
-            Console.WriteLine("NS; {0}; Library; {1}; Occurence; {2}; Confidence; {3}", element.NamespaceName, element.Library, element.Occurences, element.Confidence);
+            Console.WriteLine("NS; {0}; Library; {1}; Occurence; {2}; Confidence; {3}", element.Value.Namespace, element.Value.LibName, element.Value.Occurrences, element.Value.Similarity);
         }
-    }
-
-
-    private List<RecognizerResult> FilterMatches(
-        List<(FunctionSignature Provided, FunctionSignature Database, double Confidence)> similarBySimHash,
-        int minOccurrence)
-    {
-        var preFiltered = similarBySimHash
-            .GroupBy(x => new { x.Database.LibName, x.Database.Version })
-            .Where(group => group.Count() >= minOccurrence)
-            .ToList();
-
-        var intermediateResults = new List<RecognizerResult>();
-        foreach (var group in preFiltered)
+        Console.WriteLine("~~~~~ SIMHASH ~~~~~~~");
+        foreach (var element in mostLikelyVersionsSimHashes)
         {
-            foreach (var signature in group)
-            {
-                var result = new RecognizerResult(
-                    group.Key.LibName,
-                    signature.Database.Namespace,
-                    signature.Database.Version,
-                    signature.Confidence,
-                    group.Count());
-
-                intermediateResults.Add(result);
-            }
+            Console.WriteLine("NS; {0}; Library; {1}; Occurence; {2}; Confidence; {3}", element.Value.Namespace, element.Value.LibName, element.Value.Occurrences, element.Value.Similarity);
         }
-
-        var recognizerResults = intermediateResults
-            .GroupBy(x => x.Library)
-            .Select(libraryGroup => libraryGroup
-                .GroupBy(x => x.Version)
-                .OrderByDescending(versionGroup => versionGroup.Average(x => x.Confidence))
-                .First()
-                .First())
-            .ToList();
-
-        return recognizerResults;
-    }
-
-    private async Task<List<(FunctionSignature signatureFromFile, FunctionSignature similarDatabaseSignature, double Similarity)>> FindSimilarSimHash(
-        List<FunctionSignature> functionSignaturesFromFile, double minSimilarity, CancellationToken cancellationToken)
-    {
-        var concurrentBag = new ConcurrentBag<(FunctionSignature signatureFromFile, FunctionSignature similarDatabaseSignature, double)>();
-        await Parallel.ForEachAsync(functionSignaturesFromFile, cancellationToken, (signatureFromFile, token) =>
-        {
-            var similarDatabaseSignatures = DataSet.Where(x =>
-                SimHash.SimilarityPercentage(x.SignatureSimhash, signatureFromFile.SignatureSimhash) > minSimilarity).ToList();
-            foreach (var similarDatabaseSignature in similarDatabaseSignatures)
-            {
-                concurrentBag.Add((signatureFromFile, similarDatabaseSignature, SimHash.SimilarityPercentage(similarDatabaseSignature.SignatureSimhash, signatureFromFile.SignatureSimhash)));
-            }
-            
-            return ValueTask.CompletedTask;
-        });
-        return concurrentBag.ToList();
     }
     
-    private async Task<List<(FunctionSignature signatureFromFile, FunctionSignature similarDatabaseSignature, double Similarity)>> FindSimilarMinHash(
-        List<FunctionSignature> functionSignaturesFromFile, double minSimilarity, CancellationToken cancellationToken)
+    Dictionary<string, (string LibName, string? Namespace, string Version, double Similarity, int Occurrences)>
+        GetMostLikelyVersions(
+            Dictionary<string, List<(string? Namespace, string LibName, string Version, double Similarity)>> extractedFeatures, int minOccurrences)
     {
-        var concurrentBag = new ConcurrentBag<(FunctionSignature signatureFromFile, FunctionSignature similarDatabaseSignature, double Similarity)>();
-        await Parallel.ForEachAsync(functionSignaturesFromFile, cancellationToken, (signatureFromFile, token) =>
+        var mostLikelyVersions = new Dictionary<string, (string LibName, string? Namespace, string Version, double Similarity, int occurrences)>();
+
+        foreach (var feature  in extractedFeatures)
         {
-            var similarDatabaseSignatures = DataSet.Where(x =>
-                MinHash.GetSimilarity(x.SignatureMinhash, signatureFromFile.SignatureMinhash) > minSimilarity).ToList();
-            foreach (var similarDatabaseSignature in similarDatabaseSignatures)
+            var groupedVersions = feature.Value.GroupBy(x => x.Version);
+            var versionStats = groupedVersions.Select(g => (
+                Version: g.Key,
+                Similarity: g.Average(x => x.Similarity),
+                Occurrences: g.Count()
+            ));
+
+            var filteredVersions = versionStats.Where(x => x.Occurrences >= minOccurrences);
+
+            var valueTuples = filteredVersions as (string Version, double Similarity, int Occurrences)[] ?? filteredVersions.ToArray();
+            if (valueTuples.Any())
             {
-                concurrentBag.Add((signatureFromFile, similarDatabaseSignature, MinHash.GetSimilarity(similarDatabaseSignature.SignatureMinhash, signatureFromFile.SignatureMinhash)));
+                var maxOccurrences = valueTuples.Max(x => x.Occurrences);
+                var mostLikelyVersion = valueTuples.FirstOrDefault(x => x.Occurrences == maxOccurrences);
+
+                if (mostLikelyVersion != default)
+                {
+                    mostLikelyVersions[feature.Key] = (
+                        feature.Key,
+                        feature.Value.First().Namespace,
+                        mostLikelyVersion.Version,
+                        mostLikelyVersion.Similarity,
+                        mostLikelyVersion.Occurrences
+                    );
+                }
             }
+        }
 
-            return ValueTask.CompletedTask;
-        });
-        return concurrentBag.ToList();
+        return mostLikelyVersions;
     }
-
-    public record RecognizerResult(string Library, string? NamespaceName, string Version, double Confidence, int Occurences);
 }
